e20aa1671af1712cde5cedad6902defb2babc396
Laiba-Hashmi <hashmilaiba1225@gmail.com>
2025-04-28 00:17:21 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+let age1 = parseInt(prompt ("age of student 1"));
+console.log(`student 1 age : ${age1}`);
+let age2 = parseInt(prompt ("age of student 2"));
+console.log(`student 2 age : ${age2}`);
+let age3 = parseInt(prompt ("age of student 3"));
+console.log(`student 3 age : ${age3}`);
+let age4 = parseInt(prompt ("age of student 4"));
+console.log(`student 4 age : ${age4}`);
+let age5 = parseInt(prompt ("age of student 5"));
+console.log(`student 5 age : ${age5}`);
+let age6 = parseInt(prompt ("age of student 6"));
+console.log(`student 6 age : ${age6}`);
+let age7 = parseInt(prompt ("age of student 7"));
+console.log(`student 7 age : ${age7}`);
+let age8 = parseInt(prompt ("age of student 8"));
+console.log(`student 8 age : ${age8}`);
+let age9 = parseInt(prompt ("age of student 9"));
+console.log(`student 9 age : ${age9}`);
+let age10 = parseInt(prompt ("age of student 10"));
+console.log(`student 10 age : ${age10}`);
+let totalAge = age1+age2+age3+age4+age5+age6+age7+age8+age9+age10
+let averageAge = totalAge/10
+console.log(`The average age of 10 students is: ${averageAge}`);
